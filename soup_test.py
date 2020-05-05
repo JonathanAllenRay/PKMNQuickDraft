ae46ff1e3ab502af4880cbd6efd1a0deb69a19fb
JonathanAllenRay <jonathanallenray1@gmail.com>
2020-05-04 21:36:00 -0400

    uhh is this on

---
+++ soup_test.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import sys
+
+
+
+def main():
+    page = requests.get("https://www.smogon.com/dex/sm/pokemon/articuno/")
+    status = page.status_code
+    soup = BeautifulSoup(page.content, 'html.parser')
+    scripts = soup.find_all('script')
+    body = scripts[0]
+    body_text = str(body)
+    lines = body_text.splitlines()
+    info_line = lines[1]
+    split_line = info_line.split("= ")
+    json_dict = json.loads(split_line[1])
+    #print(type(json_dict['injectRpcs'][2]))
+    #print(len(json_dict['injectRpcs'][2]))
+
+    #print(type((json_dict['injectRpcs'][2])[1]))
+
+
+    #print(((json_dict['injectRpcs'][2])[1])['strategies'])
+
+    #print(len(((json_dict['injectRpcs'][2])[1])['strategies'])) give us a list of dict
+
+    strats_list = (((json_dict['injectRpcs'])[2])[1])['strategies']
+
+    print(json.dumps(strats_list[0]))
+
+
+
+def old():
+    print("TESTING")
+    page = requests.get("https://www.smogon.com/dex/sm/pokemon/articuno/")
+    print("STATUS: " + str(page.status_code))
+    soup = BeautifulSoup(page.content, 'html.parser')
+    soup_children = list(soup.children)
+    html = soup_children[3] #3 is the index of the html apparently?
+    html_children = list(html.children)  
+    
+    good_stuff = html_children[1]
+    
+    good_children = good_stuff.find_all('script')
+    print(len(good_children))
+
+
+def test(str): 
+    print(str)
+
+main()
